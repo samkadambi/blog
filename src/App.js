a6a1ab1387bc9005827b900736172c1a3200e177
Maanas Kompalli <maanask@umich.edu>
2023-02-15 08:44:32 +0100

    Brussels

--- src/App.js
+++ src/App.js
@@ -13,16 +13,28 @@ import chefstreet from "../src/images/SamarthMorocco/chefstreet.JPG";
 import food from "../src/images/SamarthMorocco/food.JPG";
 import weirdcafe from "../src/images/SamarthMorocco/weirdcafe.JPG";
 import chefsunset from "../src/images/SamarthMorocco/chefsunset.JPG";
-import bday1 from "../src/images/Maanas21st/bday1.jpg"
-import bday2 from "../src/images/Maanas21st/bday2.jpg"
-import bday3 from "../src/images/Maanas21st/bday3.jpg"
-import bday4 from "../src/images/Maanas21st/bday4.jpg"
-import bday5 from "../src/images/Maanas21st/bday5.jpg"
-import bday6 from "../src/images/Maanas21st/bday6.jpg"
-import bday7 from "../src/images/Maanas21st/bday7.jpg"
+import bday1 from "../src/images/Maanas21st/bday1.jpg";
+import bday2 from "../src/images/Maanas21st/bday2.jpg";
+import bday3 from "../src/images/Maanas21st/bday3.jpg";
+import bday4 from "../src/images/Maanas21st/bday4.jpg";
+import bday5 from "../src/images/Maanas21st/bday5.jpg";
+import bday6 from "../src/images/Maanas21st/bday6.jpg";
+import bday7 from "../src/images/Maanas21st/bday7.jpg";
+import bday8 from "../src/images/Maanas21st/RAGER.MOV";
+import brussels1 from "../src/images/MaanasBrussels/brussels1.jpg";
+import brussels2 from "../src/images/MaanasBrussels/brussels2.jpg";
+import brussels3 from "../src/images/MaanasBrussels/brussels3.jpg";
+import brussels4 from "../src/images/MaanasBrussels/brussels4.jpg";
+import brussels5 from "../src/images/MaanasBrussels/brussels5.jpg";
+import brussels6 from "../src/images/MaanasBrussels/brussels6.jpg";
+import brussels7 from "../src/images/MaanasBrussels/brussels7.jpg";
+import brussels8 from "../src/images/MaanasBrussels/brussels8.jpg";
+import brussels9 from "../src/images/MaanasBrussels/brussels9.jpg";
+import brussels10 from "../src/images/MaanasBrussels/brussels10.jpg";
 import { ReactPhotoCollage } from "react-photo-collage";
 import Button from '@mui/material/Button';
 import NightlifeIcon from '@mui/icons-material/Nightlife';
+
 
 const morocco_collage = {
   width: '600px',
@@ -51,9 +63,48 @@ const bday_collage = {
     { source: bday3 },
     { source: bday4 },
     { source: bday7 },
+    { source: bday8 },
   ],
   showNumOfRemainingPhotos: true
 };
+
+const brussels_collage = {
+  width: '600px',
+  height: ['250px', '170px'],
+  layout: [1, 4],
+  photos: [
+    { source: brussels1 },
+    { source: brussels2 },
+    { source: brussels3 },
+    { source: brussels4 },
+    { source: brussels5 },
+    { source: brussels6 },
+    { source: brussels7 },
+    { source: brussels8 },
+    { source: brussels9 },
+    { source: brussels10 },
+  ],
+  showNumOfRemainingPhotos: true
+};
+
+const italy_collage = {
+  width: '600px',
+  height: ['250px', '170px'],
+  layout: [1, 4],
+  photos: [
+    { source: bday6 },
+    { source: bday5 },
+    { source: bday1 },
+    { source: bday2 },
+    { source: bday3 },
+    { source: bday4 },
+    { source: bday7 },
+    { source: bday8 },
+  ],
+  showNumOfRemainingPhotos: true
+};
+
+
 
 function App() {
   return (
@@ -142,12 +193,12 @@ function App() {
           </Popup>
         </VerticalTimelineElement>
         <VerticalTimelineElement
-        className="Samarth-Morocco"
-        date="December 2022"
+        className="MaanasBrussels"
+        date="January 27th - 29th"
         iconStyle={{ background: "#e9d35b", color: "green"}}
         icon={<WbSunnyIcon/>}
         >
-          <h1> Maanas has a vaj</h1>
+          <h1> Maanas touches down in Brussels</h1>
           <Popup trigger={<Button  variant="contained" className="button"> click for a milli </Button>} 
           modal
           nested
@@ -157,23 +208,56 @@ function App() {
                 <button className="close" onClick={close}>
                   &times;
                 </button>
-                <div className="header"> BRUH </div>
+                <div className="header"> Kevin de Bruyne is my DADDY!!!! </div>
                 <div className="content">
                   {' '}
-                  EPIC BRUH MOMENT
+                  Ah yes, Belgium. The land of waffles, beer, fries, chocolate, and a random statue of a baby that has been pissing 
+                  since my parents visited it 25 years ago. And no, I'm not kidding about that last one, just Google the "Mannekin Piss" and
+                  take a look for yourself. We went there by bus, which 
+                  took about 4 hours. Even so, we stopped at the 2 hour mark to get food and drinks which instantly had me thinking about how the French
+                  really don't road trip much. A 4 hour drive back home is usually done without any stops. But why would the French need to travel anyway?
+                  Especially if you live within the vicinity of Paris, there is always something to do. It's a complete 180 from Michigan where people 
+                  literally walk down the aisles of grocery stores as their concept of "fun."
+                  Anyways, to describe my time in Brussels in one word, I'd go with "boolery." We got there around 11ish at night which is around the time
+                  that the energy of the city started to pick up. We were living in a hostel not too far with a beautiful view of the river Senne just a couple 
+                  minutes away. As soon as we got off the bus and put our bags in the room, we got straight to it. We went to one of the best fry shops in town 
+                  called "Fritland" and the boolery commenced. One of the items on the menu was called the "Mitrailette" which, according to one of the French
+                  students on the trip, meant "AK-47." So, naturally, as one of the few Americans on the trip, I asked for just that. I blessed them with the most
+                  beautiful sentence in Franglais that I could have ever crafted: "Bonsoir monsieur, je voudrais le AK-47, s'il vous plait." The confused looks I got
+                  were absolutely priceless. Eventually one of them caught on and told me that they ran out of ingredients for that menu item, so I settled for some 
+                  fries with some spicy Samurai sauce. Best I've ever had. Later that same night we went to one of the clubs in town called "La Machine" and the vibes were
+                  instantly there. I heard them playing "Goosebumps" by Travis Scott from outside the club and I rushed in, nobody checking IDs at the front. We stayed until about 3 in the 
+                  morning and were on our way back when further clowning ensued. We came across this awkwardly placed stoplight that was facing the sidewalk, not really serving
+                  much of a purpose. So we decided to give it one. We turned the stoplight towards the street and waited for cars to come rolling through to see
+                  if they would fall for it. In the next 5 minutes it was a straight up traffic jam! I don't think I'd laughed harder in my life. The second day consisted of going
+                  to different cathedrals and other major tourist attractions including a chocolate museum and that pissing baby I was talking about earlier. Later that night, we went
+                  to the one of the biggest bars in Brussels called Delirium. I remember I had purchased a massive stein from a gift shop earlier in the day and thought to myself "Hey, why
+                  don't I put this to good use." I took it out and asked them to fill it to the brim with their best beer. The bartenders spoke amongst themselves for a minute and finally 
+                  agreed on filling it up. I don't think I had ever felt more Belgian than I did that night. The rest of the night was a blur but one thing was glued to my mind from the night.
+                  When we got out of the bar, a bunch of the French students sat down in a line and started throwing people over the top of the line, carrying them through.
+                  Just imagine someone crowdsurfing but its over a single file line, and there are multiple peoeple going through this line at the same time. I obviously joined in and took a look
+                  around. Pretty much everyone within a 100m radius had their eyes glued on this rather unconventional tradition, cheering loudly with some even joining in. The next hour or so consisted of the French students initiating the 
+                  Americans by teaching various fight songs and customs, which was an eye-opening experience to say the least. The French really know how to turn up.
+                  The last day was a bit mellow- we went to the Atomium, which is where the world fair was hosted in the 50s. I also had a dance off with a random drunk dude who was blasting one of my favorite TikTok bangers on his bluetooth speaker,
+                  the one that goes "BRRR SKIPPITY DOP DOP DOP DOP DOP YES YES YES YES SKIP SKIPPITY DIP DIPPITY DUBBLA DUBBLA DUBBLA DUBBLA YES YES YES YES"--- if you know, you know. Before long it was time to head back, and all I could think about was how much of a nose dive I took
+                  into European culture in a span of just 3 days. I ate and drank like a Belgian, and turned up like a Frenchman. I even got some of the French students to sing "Ramenez la coupe a la maison"
+                  with me on the way back. Not once did it occur to me that I may be out of place or different. This weekend, I was a European at heart. Pretty much everyone's essay for their study abroad application must have
+                  incorporated the idea of "exploring a new culture or language." The truth is, that won't just naturally happen just because you're in the vicinity of people who are of that different culture.
+                  Making a conscious effort to be open-minded and see things from a different perspective is what really elevated this weekend in my mind. I will definitely be back at some point to visit the more scenic parts of Belgium,
+                  most notably Brugge. Until then, this is Maanas, signing off.
                 </div>
-                <img src={img} alt="bruh"/>
+                <ReactPhotoCollage {...brussels_collage} />
               </div>
             )}
           </Popup>
         </VerticalTimelineElement>
         <VerticalTimelineElement
-        className="Samarth-Morocco"
-        date="December 2022"
+        className="MaanasItaly"
+        date="January 2023"
         iconStyle={{ background: "#e9d35b", color: "green"}}
         icon={<WbSunnyIcon/>}
         >
-          <h1> Maanas has a vaj</h1>
+          <h1> Maanas munches on pizza in Italy</h1>
           <Popup trigger={<Button variant="contained" className="button"> click for a milli </Button>} 
           modal
           nested
@@ -183,10 +267,10 @@ function App() {
                 <button className="close" onClick={close}>
                   &times;
                 </button>
-                <div className="header"> BRUH </div>
+                <div className="header"> Ciao! </div>
                 <div className="content">
                   {' '}
-                  EPIC BRUH MOMENT
+                  
                 </div>
                 <img src={img} alt="bruh"/>
               </div>
