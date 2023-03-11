6e86610a67a1245f9f93abb47ee22bf9094fb3a2
Samarth Kadambi <samarthk@umich.edu>
2023-03-11 10:01:24 -0500

    Fixed Brussels, finish Italy and Spain

--- src/App.js
+++ src/App.js
@@ -31,10 +31,18 @@ import brussels7 from "../src/images/MaanasBrussels/brussels7.jpg";
 import brussels8 from "../src/images/MaanasBrussels/brussels8.jpg";
 import brussels9 from "../src/images/MaanasBrussels/brussels9.jpg";
 import brussels10 from "../src/images/MaanasBrussels/brussels10.jpg";
+import barca from "../src/images/Spain/barca.jpeg";
+import bball from "../src/images/Spain/bball.jpeg";
+import chill from "../src/images/Spain/chill.jpeg";
+import club from "../src/images/Spain/club.jpeg";
+import lisbon from "../src/images/Spain/lisbon.jpeg";
+import madrid from "../src/images/Spain/madrid.jpeg";
+import strat from "../src/images/Spain/strat.jpeg";
 import { ReactPhotoCollage } from "react-photo-collage";
 import Button from '@mui/material/Button';
 import NightlifeIcon from '@mui/icons-material/Nightlife';
-
+import AirplanemodeActiveIcon from '@mui/icons-material/AirplanemodeActive';
+import LocalPizzaIcon from '@mui/icons-material/LocalPizza';
 
 const morocco_collage = {
   width: '600px',
@@ -104,6 +112,21 @@ const italy_collage = {
   showNumOfRemainingPhotos: true
 };
 
+const spain_collage = {
+  width: '600px',
+  height: ['250px', '170px'],
+  layout: [1, 4],
+  photos: [
+    { source: barca },
+    { source: bball },
+    { source: chill },
+    { source: club },
+    { source: lisbon },
+    { source: madrid },
+    { source: strat },
+  ],
+  showNumOfRemainingPhotos: true
+}
 
 
 function App() {
@@ -127,7 +150,7 @@ function App() {
         <VerticalTimelineElement
         className="Samarth-Morocco"
         date="Dec. 2022 - Jan. 2023"
-        iconStyle={{ background: "white", color: "orange"}}
+        iconStyle={{ background: "white", color: "blue"}}
         icon={<WbSunnyIcon/>}
         >
           <h1>Samarth ran up morocco</h1>
@@ -193,12 +216,12 @@ function App() {
           </Popup>
         </VerticalTimelineElement>
         <VerticalTimelineElement
-        className="Samarth-Morocco"
-        date="December 2022"
-        iconStyle={{ background: "#e9d35b", color: "green"}}
-        icon={<WbSunnyIcon/>}
+        className="MaanasBrussels"
+        date="January 27th - 29th"
+        iconStyle={{ background: "white", color: "blue"}}
+        icon={<NightlifeIcon/>}
         >
-          <h1> Maanas has a vaj</h1>
+          <h1> Maanas touches down in Brussels</h1>
           <Popup trigger={<Button  variant="contained" className="button"> click for a milli </Button>} 
           modal
           nested
@@ -208,23 +231,50 @@ function App() {
                 <button className="close" onClick={close}>
                   &times;
                 </button>
-                <div className="header"> BRUH </div>
+                <div className="header"> Kevin de Bruyne is my DADDY!!!! </div>
                 <div className="content">
                   {' '}
-                  EPIC BRUH MOMENT
+                  Ah yes, Belgium. The land of waffles, beer, fries, chocolate, and a random statue of a baby that has been pissing 
+                  since my parents visited it 25 years ago. And no, I'm not kidding about that last one, just Google the "Mannekin Piss" and
+                  take a look for yourself.
+                  Anyways, to describe my time in Brussels in one word, I'd go with "boolery." We were living in a hostel not too far with a beautiful view of the river Senne just a couple 
+                  minutes away. As soon as we got off the bus and put our bags in the room, we got straight to it. We went to one of the best fry shops in town 
+                  called "Fritland" and the boolery commenced. One of the items on the menu was called the "Mitrailette" which, according to one of the French
+                  students on the trip, meant "AK-47." So, naturally, as one of the few Americans on the trip, I asked for just that. I blessed them with the most
+                  beautiful sentence in Franglais that I could have ever crafted: "Bonsoir monsieur, je voudrais le AK, s'il vous plait." The confused looks I got
+                  were absolutely priceless. Eventually one of them caught on and told me that they ran out of ingredients for that menu item, so I settled for some 
+                  fries with some spicy Samurai sauce. Best I've ever had. Later that same night we went to one of the clubs in town called "La Machine" and the vibes were
+                  instantly there. I heard them playing "Goosebumps" by Travis Scott from outside the club and I rushed in, nobody checking IDs at the front. We stayed until about 3 in the 
+                  morning and were on our way back when further clowning ensued. We came across this awkwardly placed stoplight that was facing the sidewalk, not really serving
+                  much of a purpose. So we decided to give it one. We turned the stoplight towards the street and waited for cars to come rolling through to see
+                  if they would fall for it. In the next 5 minutes it was a straight up traffic jam! I don't think I'd laughed harder in my life. The second day consisted of going
+                  to different cathedrals and other major tourist attractions including a chocolate museum and that pissing baby I was talking about earlier. Later that night, we went
+                  to the one of the biggest bars in Brussels called Delirium. I remember I had purchased a massive stein from a gift shop earlier in the day and thought to myself "Hey, why
+                  don't I put this to good use." I took it out and asked them to fill it to the brim with their best beer. The bartenders spoke amongst themselves for a minute and finally 
+                  agreed on filling it up. I don't think I had ever felt more Belgian than I did that night. The rest of the night was a blur but one thing was glued to my mind from the night.
+                  When we got out of the bar, a bunch of the French students sat down in a line and started throwing people over the top of the line, carrying them through.
+                  Just imagine someone crowdsurfing but its over a single file line, and there are multiple peoeple going through this line at the same time. I obviously joined in and took a look
+                  around. Pretty much everyone within a 20m radius had their eyes glued on this rather unconventional tradition, cheering loudly with some even joining in. The next hour or so consisted of the French students initiating the 
+                  Americans by teaching various fight songs and customs, which was an eye-opening experience to say the least. The French really know how to turn up.
+                  The last day was a bit mellow- we went to the Atomium, which is where the world fair was hosted in the 50s. I also had a dance off with a random drunk dude who was blasting one of my favorite TikTok bangers on his bluetooth speaker,
+                  the one that goes "BRRR SKIPPITY DOP DOP DOP DOP DOP YES YES YES YES SKIP SKIPPITY DIP DIPPITY DUBBLA DUBBLA DUBBLA DUBBLA YES YES YES YES"--- if you know, you know. Before long it was time to head back, and all I could think about was how much of a nose dive I took
+                  into European culture in a span of just 3 days. I ate and drank like a Belgian, and turned up like a Frenchman. I even got some of the French students to sing "Ramenez la coupe a la maison"
+                  with me on the way back.
+                  I will definitely be back at some point to visit the more scenic parts of Belgium,
+                  most notably Brugge. Until next time, this is Maanas, signing off.
                 </div>
-                <img src={img} alt="bruh"/>
+                <ReactPhotoCollage {...brussels_collage} />
               </div>
             )}
           </Popup>
         </VerticalTimelineElement>
         <VerticalTimelineElement
-        className="Samarth-Morocco"
-        date="December 2022"
-        iconStyle={{ background: "#e9d35b", color: "green"}}
-        icon={<WbSunnyIcon/>}
+        className="MaanasItaly"
+        date="January 2023"
+        iconStyle={{ background: "white", color: "blue"}}
+        icon={<LocalPizzaIcon/>}
         >
-          <h1> Maanas has a vaj</h1>
+          <h1> Maanas a munch</h1>
           <Popup trigger={<Button variant="contained" className="button"> click for a milli </Button>} 
           modal
           nested
@@ -234,12 +284,42 @@ function App() {
                 <button className="close" onClick={close}>
                   &times;
                 </button>
-                <div className="header"> BRUH </div>
+                <div className="header"> Ciao! </div>
                 <div className="content">
                   {' '}
-                  EPIC BRUH MOMENT
+                  
                 </div>
                 <img src={img} alt="bruh"/>
+              </div>
+            )}
+          </Popup>
+        </VerticalTimelineElement>
+
+        <VerticalTimelineElement
+        className="Samarth-Maanas-Spain"
+        date="February 2023"
+        iconStyle={{ background: "white", color: "blue"}}
+        icon={<AirplanemodeActiveIcon/>}
+        >
+          <h1>Spain!</h1>
+          <Popup trigger={<Button variant="contained" className="button"> click for a milli </Button>} 
+          modal
+          nested
+          >
+            {close => (
+              <div className="modal">
+                <button className="close" onClick={close}>
+                  &times;
+                </button>
+                <div className="header"> Meet up </div>
+                <div className="content">
+                  {' '}
+                  Over spring break, We met up in Spain for a week of great fun. Our first stop was Madrid, and we immediately hit the club. We turnt up in a 7 story joint, et bien sur, we rizzed.
+                  We visited some parks, museums, and bars; you know, the touristy shit. Lemme tell you tho, these Europeans stink. I swear they do not shower and just douse themselves in cologne. Imagine mixing straight must with dior sauvage.
+                  Another highlight of Madrid was a pub crawl we ventured on. Maanas almost got finessed by a stripper, but I (Sam) saved him. We met a dutch feller, shared a few drinks, and clowned on him a little. We then met up again Barcelona.
+                  We saw some old friends, visited the Sagrada Familia, and clubbed again. I also went to Lisbon for a day and half. Pretty chiller city; super hilly, pretty, and all around ferda. Europe was one hell of an experience, and I hope to be back there soon
+                </div>
+                <ReactPhotoCollage {...spain_collage} />
               </div>
             )}
           </Popup>
